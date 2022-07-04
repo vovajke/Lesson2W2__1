2204a9ae0d27acafd96d6f649c48edd9376b0c91
Vladimir Gerasimov <vovagerasimove2011@yandex.ru>
2022-07-04 15:06:39 +0300

    Добавьте файлы проекта.

---
+++ Lesson2W2(1)/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lesson2W2_1_
+{
+    partial class Program
+    {
+        public class Account
+        {
+            int _number;
+
+            decimal _balance;
+
+            TypeOfBankAccount _typeAccount;
+
+            public int InfoNumber()
+            {
+                return _number;
+            }
+
+            public decimal InfoBalance()
+            {
+                return _balance;
+            }
+
+            public TypeOfBankAccount InfoTypeAccount()
+            {
+                return _typeAccount;
+            }
+
+            public void FillNumber(int number)
+            {
+                _number = number;
+            }
+
+            public void FillBalance(decimal balance)
+            {
+                _balance = balance;
+
+            }
+
+            public void FillTypeAccount(TypeOfBankAccount typeAccount)
+            {
+                _typeAccount = typeAccount;
+            }
+
+            public void FullInfo()
+            {
+                Console.WriteLine($"Тип счёта: {_typeAccount}, Номер счёта: {_number}, Баланс: {_balance}");
+            }
+            static void Main(string[] args)
+            {
+                Random random = new Random();
+
+                Account account = new Account();
+
+                account.FillTypeAccount(TypeOfBankAccount.DEBIT);
+
+                account.FillNumber(random.Next(1, 99999999));
+
+                account.FillBalance(100000);
+
+                account.FullInfo();
+            }
+            public enum TypeOfBankAccount
+            {
+                DEBIT,
+                CREDIT,
+                DEPOSIT,
+                BUDGET
+            }
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Lesson2W2(1)/TypeOfBankAccount.cs
@@ -0,0 +1,53 @@
+﻿namespace Lesson2W2_1_
+{
+    partial class Program
+    {
+        public enum TypeOfBankAccount
+        {
+
+        }
+    }
+}
+        
+        
+        
+      
+        
+        
+              
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+            
+    
+
+
+        
+            
+     
+    
+    
+    
+
+
+        
+    
+
